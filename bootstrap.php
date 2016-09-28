9688ed3d784fd933e98f1ea67e73438ea06645cd
martinchos93 <martinsanchez9314@gmail.com>
2016-09-28 20:27:05 -0300

    doctrine installation

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+// bootstrap.php
+	require_once("vendor/autoload.php");
+
+	use Doctrine\ORM\Tools\Setup;
+	use Doctrine\ORM\EntityManager;
+
+	$paths = array("/app/entity");
+	$isDevMode = false;
+
+	// the connection configuration
+	$dbParams = array(
+	    'driver'   => 'pdo_mysql',
+	    'user'     => 'root',
+	    'password' => 'ty6ncufe5',
+	    'dbname'   => 'ISOOPRODB',
+	);
+
+	$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+	$entityManager = EntityManager::create($dbParams, $config);
+
+	
+?>
