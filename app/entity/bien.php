5244488cadae92896d49c4335b01d68276d1507c
martinchos93 <martinsanchez9314@gmail.com>
2016-09-29 21:37:24 -0300

    ultimos cambios

---
+++ app/entity/bien.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	/**
+	* 
+	*/
+	class ClassName	{
+		private $id;
+		private $nombre;
+		private $descripcion;
+		private $mubroId;
+		private $modelo;
+		private $valor;
+	}
+
+ ?>
---
+++ app/entity/mantenimiento.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	/**
+	* 
+	*/
+	class mantenimiento	
+	{
+		private $id;
+		private $idBien;
+		private $descripcion;
+		private $fecha;
+		private $tipo;
+		private $idUsuario;
+		private $importe;
+	}
+
+ ?>
